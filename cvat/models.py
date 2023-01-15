4f420c0a3fcc5c3d4a83a2bb31e9025545312194
sg-object <49360550+sg-object@users.noreply.github.com>
2023-01-15 23:36:38 +0900

    Update models.py

--- cvat/models.py
+++ cvat/models.py
@@ -15,3 +15,17 @@ class SwaggerToken(models.Model):
     
     class Meta:
         db_table = 'swagger_token'
+
+class Label(models.Model):
+    task = models.ForeignKey(Task, null=True, blank=True, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, null=True, blank=True, on_delete=models.CASCADE)
+    name = SafeCharField(max_length=64)
+    color = models.CharField(default='', max_length=8)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        default_permissions = ()
+        unique_together = ('task', 'name')
+        ordering = ['id']
